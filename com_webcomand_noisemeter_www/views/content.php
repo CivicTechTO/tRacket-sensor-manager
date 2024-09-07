61838a1e60965c00836fa49285ae9a6bbc503512
Mitch Bechtel <mitch.bechtel@comandsolutions.com>
2024-09-07 17:41:12 -0400

    Create content.php

---
+++ com_webcomand_noisemeter_www/views/content.php
@@ -0,0 +1,4 @@
+#/ insert a web page wrapper that all other views will sit inside, for the website navigation and a basic layout
+#PATHTYPE(Root)
+#INSERT(SitesPage+,OID=98957,HTML,TRUE)
+#ENDPATHTYPE
