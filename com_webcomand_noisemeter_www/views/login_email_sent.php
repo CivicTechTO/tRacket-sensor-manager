9ba5a069130d0815f7ec395c358e8cbbd252c80e
Mitch Bechtel <mitch.bechtel@comandsolutions.com>
2024-09-07 17:43:36 -0400

    Create login_email_sent.php

---
+++ com_webcomand_noisemeter_www/views/login_email_sent.php
@@ -0,0 +1,8 @@
+        <div class="login">
+            <h2>Login</h2>
+            <form method="post" action="<?= $base_url ?>device_manager/login">
+                <p>Check your email to complete your login.</p>
+                <p>Didn't receive the email?  Wait at least 30 seconds and also check your Junk Mail folder.</p>
+                <p>If you still haven't received an email, you can <a href="<?= $base_url ?>device_manager/login">try again</a>.
+            </form>
+        </div>
